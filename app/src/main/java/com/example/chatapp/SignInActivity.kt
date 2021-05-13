b7311f755fd8f4045eae2b9e09a77d0c0c862ac3
AaronWall97 <aaron.wall@rocketmail.com>
2021-05-13 15:15:22 +0100

    update

--- app/src/main/java/com/example/chatapp/SignInActivity.kt
+++ app/src/main/java/com/example/chatapp/SignInActivity.kt
@@ -1,11 +1,35 @@
 package com.example.chatapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+
+    private var googleSignInClient : GoogleSignInClient? = null
+    private var fireBaseAuth : FirebaseAuth? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        fireBaseAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this@SignInActivity,gso)
+
+        set
+
+
+
+
     }
 }
