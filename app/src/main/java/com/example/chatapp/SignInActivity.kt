e99db3fb43fd99ba99b4dba01511c00bd7293e0a
AaronWall97 <aaron.wall@rocketmail.com>
2021-05-15 14:00:52 +0100

    update

--- app/src/main/java/com/example/chatapp/SignInActivity.kt
+++ app/src/main/java/com/example/chatapp/SignInActivity.kt
@@ -1,18 +1,25 @@
 package com.example.chatapp
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.auth.GoogleAuthProvider
 
 
 class SignInActivity : AppCompatActivity() {
 
     companion object {
+        private const val TAG = "SignInActivity"
         private const val RC_SIGN_IN = 1
     }
 
@@ -45,4 +52,38 @@ class SignInActivity : AppCompatActivity() {
 
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            if(resultCode == Activity.RESULT_OK){
+                val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+                try{
+                 val account = task.getResult(ApiException::class.java)
+                    firebaseAuthWithGoogle(account!!)
+                }catch (e : ApiException){
+                    Log.e(TAG,"Google Sign in failed $e")
+                    Toast.makeText(this@SignInActivity, "Google Sign in failed", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(account : GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+
+        fireBaseAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this@SignInActivity){task ->
+
+                    if (task.isSuccessful){
+                        startActivity(Intent(this@SignInActivity, MainActivity::class.java))
+                        finish()
+                    }
+                }
+
+    }
+
 }
